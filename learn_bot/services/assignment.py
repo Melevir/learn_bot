e5102a4cab2e92b90cdf872ebe0c491d99488864
Ilya Lebedev <melevir@gmail.com>
2023-05-15 19:29:10 +0400

    get tg chat id from user column

--- learn_bot/services/assignment.py
+++ learn_bot/services/assignment.py
@@ -1,10 +1,20 @@
+import logging
+
 from learn_bot.bot import Bot
 from learn_bot.db import Assignment
+from learn_bot.screenplay.db.fetchers import fetch_user_by_telegram_nickname
+
+logger = logging.getLogger(__name__)
 
 
 def handle_new_assignment(assignment: Assignment, bot: Bot) -> None:
-    curator = assignment.student.group.curator
-    bot.send_message(curator.telegram_chat_id, f"{assignment.student.full_name} сдал работу на проверку")
+    with bot.get_session() as session:
+        curator = assignment.student.group.curator
+        curator_user = fetch_user_by_telegram_nickname(curator.telegram_nickname, session)
+    if curator_user and curator_user.telegram_chat_id:
+        bot.send_message(curator_user.telegram_chat_id, f"{assignment.student.full_name} сдал работу на проверку")
+    else:
+        logger.error(f"Not found curator telegram_chat_id ({curator.telegram_nickname})")
 
 
 def handle_assignment_checked(assignment: Assignment, bot: Bot) -> None:
@@ -19,4 +29,9 @@ def handle_assignment_checked(assignment: Assignment, bot: Bot) -> None:
             f"{curator.first_name} проверил твоё задание ({assignment.url}). "
             f"Комментарии в пул-реквеста на Гитхабе"
         )
-    bot.send_message(assignment.student.telegram_chat_id, message)
+    with bot.get_session() as session:
+        student_user = fetch_user_by_telegram_nickname(assignment.student.telegram_nickname, session)
+    if student_user and student_user.telegram_chat_id:
+        bot.send_message(student_user.telegram_chat_id, message)
+    else:
+        logger.error(f"Not found student telegram_chat_id ({assignment.student.telegram_nickname})")
