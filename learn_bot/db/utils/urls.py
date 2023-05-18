338f9ed257cb345614617c66c8c5a8b61380533b
Ilya Lebedev <melevir@gmail.com>
2023-05-18 10:13:59 +0400

    check for github domain

--- learn_bot/db/utils/urls.py
+++ learn_bot/db/utils/urls.py
@@ -1,17 +1,28 @@
+import logging
+
 import requests
 import validators
 
 
+logger = logging.getLogger(__name__)
+
+
 def is_valid_github_url(url: str) -> bool:
-    return bool(validators.url(url))
+    return bool(validators.url(url)) and is_github_url(url)
 
 
 def is_url_accessible(url: str) -> bool:
     try:
         response = requests.get(url)
-    except requests.exceptions.RequestException:
+    except requests.exceptions.RequestException as e:
+        logger.exception(e)
         return False
     return bool(response)
+
+
+def is_github_url(url: str) -> bool:
+    splitted_url = url.split('/')
+    return len(splitted_url) > 2 and splitted_url[2] == "github.com"
 
 
 def is_github_pull_request_url(url: str) -> bool:
